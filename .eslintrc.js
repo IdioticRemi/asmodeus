06bfbee75a56e6f5258f39775f658265d9768c9f
Kizuru <fr.kizuru@gmail.com>
2020-01-08 21:21:22 +0100

    Updated .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,10 @@ module.exports = {
   extends: "bamboo",
   rules: {
 	"linebreak-style": 0,
-	"@typescript-eslint/ban-ts-ignore": 0
+	"no-eval": 0,
+	"radix": 0,
+
+	"@typescript-eslint/ban-ts-ignore": 0,
   },
   parserOptions: {
     project: "./tsconfig.json"
